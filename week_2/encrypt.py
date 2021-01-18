5a1ae4a4c1664e7b18381e5c5bcf378f1c7d866e
Ella Neumann <neumane4@wwu.edu>
2021-01-17 20:13:27 -0800

    added encrypt

--- week_2/encrypt.py
+++ week_2/encrypt.py
@@ -1,12 +1,25 @@
 
-the_file = open('trial.txt','r+')
-while True:
-    line = the_file.readline()
-    for x in line:
-        data = x.replace(x, chr(ord(x) - 2))
-        the_file.write(data)
-        print(x)
-    if not line:
-        break
+def encrypt():
+    the_file = open('trial.txt','r+')
+    while True:
+        line = the_file.readline()
+        for x in line:
+            data = x.replace(x, chr(ord(x) - 2))
+            the_file.write(data)
+        if not line:
+            break
         
-the_file.close()
+    the_file.close()
+
+def decrypt():
+    the_file = open('trial.txt','r+')
+    while True:
+        line = the_file.readline()
+        for x in line:
+            data = x.replace(x, chr(ord(x) + 2))
+            the_file.write(data)
+        if not line:
+            break
+        
+    the_file.close()    
+encrypt()   
